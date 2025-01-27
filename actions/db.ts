89f2cfa639940d8a93804b7843785f22880f7a24
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-01-27 19:34:50 +0530

    fixed

--- actions/db.ts
+++ actions/db.ts
@@ -3,12 +3,14 @@ import mongoose from "mongoose"
 const DB = process.env.NEXT_PUBLIC_DB!;
 
 const ConnectDB = ()=>{
+    console.log("Pre db connection");
     mongoose.connect(DB).then(()=>{
-
         console.log("Database connected");
+        
     }).catch((errors)=>{
         console.log("Cannot Connect to database due to :-",errors);
     })
+    console.log("Post db connection");
 }
 
 export default ConnectDB;
