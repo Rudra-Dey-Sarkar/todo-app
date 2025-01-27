b266a47d4a57e3adb19bc34ca10e8f85d97bdb6d
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-01-27 19:45:07 +0530

    fixed

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,34 +1,34 @@
 "use server"
 import { NextRequest, NextResponse } from "next/server"
 import mongoose from "mongoose"
+import ConnectDB from "../../../../actions/db";
 
 const tasksSchema = new mongoose.Schema({
-    userId:String,
-    taskName:String,
-    date:String,
-    steps:[String],
-    notes:String,
+    userId: String,
+    taskName: String,
+    date: String,
+    steps: [String],
+    notes: String,
     important: { type: Boolean, default: false },
-    reminder:{type:Boolean, default:false},
-    status:{type:Boolean, default:false},
-}, 
-  {collection : "tasks"});
+    reminder: { type: Boolean, default: false },
+    status: { type: Boolean, default: false },
+},
+    { collection: "tasks" });
 
 const tasksModel = mongoose.models.tasks || mongoose.model("tasks", tasksSchema);
 
 
-export const POST= async (req:NextRequest)=>{
-const data = await req.json();
-
-return NextResponse.json(data);
-// try{
-//   const response = await tasksModel.find({_id:data?.id});
-//   if(response.length>0){
-//     return NextResponse.json({ status: 200, message: response });
-//   }else{
-//     return NextResponse.json({ status: 404, message: "No Tasks Available"});
-//   }
-// }catch(errors){
-//     return NextResponse.json({ status: 400, message: errors });
-// }
+export const POST = async (req: NextRequest) => {
+    ConnectDB();
+    const data = await req.json();
+    try {
+        const response = await tasksModel.find({ _id: data?.id });
+        if (response.length > 0) {
+            return NextResponse.json({ status: 200, message: response });
+        } else {
+            return NextResponse.json({ status: 404, message: "No Tasks Available" });
+        }
+    } catch (errors) {
+        return NextResponse.json({ status: 400, message: errors });
+    }
 }
